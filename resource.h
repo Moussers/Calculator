bf89a4541ad526dc8d64bf7410d82f86d91445cb
sandCat <Sand@DESKTOP-K3SU1F7>
2025-12-27 16:26:15 +0300

    Added removed file ico format and fixed bag pucture by ico file

--- resource.h
+++ resource.h
@@ -2,11 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-
-#define FONT							8
-
+#define FONT                            8
 #define IDI_ICON1                       101
-#define IDR_FONT1						119
+#define IDR_FONT1                       119
 #define IDM_EXIT                        200
 #define IDM_SQUARE_BLUE                 201
 #define IDM_METAL_MISTRAL               202
@@ -35,7 +33,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
